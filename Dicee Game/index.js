7216b8a22e04370d685387d35ed2260db4fdcc20
Shashwat Kumar <144956225+shashwatgithubworld@users.noreply.github.com>
2024-03-18 00:36:07 +0530

    Update index.js

--- Dicee Game/index.js
+++ Dicee Game/index.js
@@ -18,6 +18,7 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
 //If player 1 wins
+
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
 }
